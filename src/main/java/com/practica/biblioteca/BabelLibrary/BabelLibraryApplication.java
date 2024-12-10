5c2af6335321d0cfe463392f587c6f7cfc2b0697
juan miguel <Miguel.Rojas@jala.university>
2024-12-09 21:05:34 -0500

    first commit

---
+++ src/main/java/com/practica/biblioteca/BabelLibrary/BabelLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.practica.biblioteca.BabelLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabelLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabelLibraryApplication.class, args);
+	}
+
+}
