3e78722916eb59895445454c5a84ad02c3614617
juan miguel <Miguel.Rojas@jala.university>
2024-12-10 03:22:00 -0500

    refactor interfaces pakage

--- src/main/java/com/practica/biblioteca/BabelLibrary/interfaces/rest/BookController.java
+++ src/main/java/com/practica/biblioteca/BabelLibrary/interfaces/rest/BookController.java
@@ -1,6 +1,8 @@
-package com.practica.biblioteca.interfaces.rest;
+package com.practica.biblioteca.BabelLibrary.interfaces.rest;
 
-import com.practica.biblioteca.domain.model.Book;
+import com.practica.biblioteca.BabelLibrary.application.usecase.AddBookUseCase;
+import com.practica.biblioteca.BabelLibrary.application.usecase.ObtainBooksUseCase;
+import com.practica.biblioteca.BabelLibrary.domain.model.Book;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,26 +11,23 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/book")
 public class BookController {
-    private final com.practica.biblioteca.application.controller.BookController bookController;
+    private final AddBookUseCase addBookUseCase;
+    private final ObtainBooksUseCase obtainBooksUseCase;
 
-    public BookController(com.practica.biblioteca.application.controller.BookController bookController) {
-        this.bookController = bookController;
+    public BookController(AddBookUseCase addBookUseCase, ObtainBooksUseCase obtainBooksUseCase) {
+        this.addBookUseCase = addBookUseCase;
+        this.obtainBooksUseCase = obtainBooksUseCase;
     }
 
     @PostMapping
-    public Book addBook(@RequestParam String title, @RequestParam String author) {
-        return bookController.addBook(title, author);
+    public ResponseEntity<String> agregarLibro(@RequestBody Book book) {
+        String id = String.valueOf(addBookUseCase.execute(book));
+        return ResponseEntity.ok("book added with ID: " + id);
     }
 
     @GetMapping
-    public List<Book> bookList(){
-        return bookController.listBooks();
-    }
-
-    @GetMapping
-    public ResponseEntity<List<Book>> getAllBooks(){
-        List<Book> books = bookController.getAllBooks();
+    public ResponseEntity<List<Book>> obtenerLibros() {
+        List<Book> books = obtainBooksUseCase.execute();
         return ResponseEntity.ok(books);
     }
-
 }
