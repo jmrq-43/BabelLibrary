c20d4d3ee387ada15bd0fe49ff616e1bf711b6db
juan miguel <Miguel.Rojas@jala.university>
2024-12-09 23:10:19 -0500

    add book entity

---
+++ src/main/java/com/practica/biblioteca/domain/entities/Book.java
@@ -0,0 +1,20 @@
+package com.practica.biblioteca.domain.entities;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Book {
+    private String name;
+    private String title;
+    private String author;
+
+    @Builder
+    public Book(String name, String title, String author) {
+        this.name = name;
+        this.title = title;
+        this.author = author;
+    }
+}
