fbf2d1a6e569690ff691d475278f0604a5723e5c
juan miguel <Miguel.Rojas@jala.university>
2024-12-10 21:40:14 -0500

    refactor domain pakage

--- src/main/java/com/practica/biblioteca/BabelLibrary/domain/Book.java
+++ src/main/java/com/practica/biblioteca/BabelLibrary/domain/Book.java
@@ -1,23 +1,13 @@
-package com.practica.biblioteca.BabelLibrary.domain.model;
+package com.practica.biblioteca.BabelLibrary.domain;
 
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Setter
 @Getter
-@NoArgsConstructor
+@Setter
 public class Book {
-    private String id;
+    private String Id;
     private String title;
     private String author;
     private int year;
-
-    @Builder
-    public Book(String id, String title, String author) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-    }
 }
--- src/main/java/com/practica/biblioteca/BabelLibrary/domain/BookRepository.java
+++ src/main/java/com/practica/biblioteca/BabelLibrary/domain/BookRepository.java
@@ -1,18 +1,6 @@
-package com.practica.biblioteca.BabelLibrary.domain.repository;
-
-import com.practica.biblioteca.BabelLibrary.domain.model.Book;
-
-import java.util.List;
-import java.util.Optional;
+package com.practica.biblioteca.BabelLibrary.domain;
 
 public interface BookRepository {
-
-    Optional<Book> fingById(String id);
-
-    List<Book> findAll();
-
-    void save(Book book);
-
-    void delete(String id);
-
+    void saveBook(Book book);
+    Book getBook(String id);
 }
